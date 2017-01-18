1cc439f9e9d402355fe29e1b8fe4b9f7638afcf4
Stefan Hertrampf <shertrampf@lufn.informatik.tu-cottbus.de>
2017-01-18 04:03:18 -0500

    Added Multimessage support. Startet to implement logging to files

--- kernel/host/xmicterm/host/xmicterm.cc
+++ kernel/host/xmicterm/host/xmicterm.cc
@@ -8,10 +8,10 @@
  * modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -20,8 +20,8 @@
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
- * Copyright 2016 Randolf Rotta, Maik Krüger, Robert Kuban, and contributors, BTU Cottbus-Senftenberg 
+ *
+ * Copyright 2016 Randolf Rotta, Maik Krüger, Robert Kuban, and contributors, BTU Cottbus-Senftenberg
  */
 #include "host/MemMapperPci.hh"
 #include "mythos/HostInfoTable.hh"
@@ -32,8 +32,34 @@
 #include <string>
 #include <cstring>
 #include <unistd.h>
+#include <unordered_map>
+#include <fstream>
 
 using namespace mythos;
+
+
+std::string DescribeIosFailure(const std::ios& stream)
+{
+  std::string result;
+
+  if (stream.eof()) {
+    result = "Unexpected end of file.";
+  }  else if (errno) {
+#if defined(__unix__)
+    // We use strerror_r because it's threadsafe.
+    // GNU's strerror_r returns a string and may ignore buffer completely.
+    char buffer[255];
+    result = std::string(strerror_r(errno, buffer, sizeof(buffer)));
+#else
+    result = std::string(strerror(errno));
+#endif
+  }
+
+  else {
+    result = "Unknown file error.";
+  }
+  return result;
+}
 
 template<typename T>
 void parsenum(char const *str, T& val)
@@ -49,7 +75,7 @@ void parsenum(char const *str, T& val)
 std::string deviceKNC(int adapter)
 {
   std::stringstream s;
-  // choose the aperture memory file with the write combining enabled. 
+  // choose the aperture memory file with the write combining enabled.
   s << "/sys/class/mic/mic" << adapter << "/device/resource0"; /// @todo or _wc ???
   return s.str();
 }
@@ -57,6 +83,37 @@ std::string deviceKNC(int adapter)
 struct InfoPtr {
   uint64_t info;
   uint64_t debug;
+};
+
+/// Class logs into files, opening new file for every sending channel id.
+class file_logger {
+public:
+  ~file_logger() {
+    for (auto f : files) {
+      f.second->close();
+      delete f.second;
+    }
+  }
+
+  void log(uint16_t vchannel, std::string msg) {
+    auto found = files.find(vchannel);
+    if (found != files.end()) {
+      *found->second << msg << "\n";
+      found->second->flush();
+    } else {
+      std::ofstream *file_stream = new std::ofstream("..\\" + std::to_string(vchannel),
+                                           std::ios::out | std::ios::trunc);
+      if (!file_stream->is_open()) {
+        std::cerr << "Could not open file " << ("..\\" + std::to_string(vchannel)) << "\n";
+        std::cerr << DescribeIosFailure(*file_stream) << "\n";
+        return;
+      }
+      files.insert({vchannel, file_stream});
+      *file_stream << msg << "\n";
+    }
+  }
+private:
+  std::unordered_map<uint16_t, std::ofstream*> files;
 };
 
 int main(int argc, char** argv)
@@ -77,29 +134,55 @@ int main(int argc, char** argv)
     cpu::clflush(info_ptr.addr());
     std::cout << "info: " << (void*)info_ptr->info << " debug: " << (void*)info_ptr->debug << " " << info_ptr->debug << std::endl;
     if (info_ptr->info == 0) sleep(1);
-  } while (info_ptr->info == 0); 
-  std::cerr << "host info is at physical address " 
+  } while (info_ptr->info == 0);
+  std::cerr << "host info is at physical address "
 	    << (void*)info_ptr->info << std::endl;
 
   auto info = micmem.access(PhysPtr<HostInfoTable>(info_ptr->info));
   cpu::clflush(info.addr());
   std::cerr << "debug stream is at physical address "
 	    << (void*)info->debugOut << std::endl;
-  
+
   auto debugOutChannel = micmem.access(PhysPtr<HostInfoTable::DebugChannel>(info->debugOut));
   PCIeRingConsumer<HostInfoTable::DebugChannel> debugOut(debugOutChannel.addr());
 
+  // collect messages until whole message arrived
+  std::unordered_map<uint16_t, std::string> messages;
+
+  // log to file
+  file_logger logger;
+
   while (true) {
     //typedef HostInfoTable::DebugChannel::handle_t handle_t;
-    /// @todo merge multi-package messages per vchannel before printing
     /// @todo log messages into a file
     auto handle = debugOut.acquireRecv();
     auto& msg = debugOut.get<DebugMsg>(handle);
-    std::cout << msg.vchannel << "(" << msg.msgbytes << "): ";
-    std::cout.write(msg.data, msg.msgbytes>DebugMsg::PAYLOAD?DebugMsg::PAYLOAD:msg.msgbytes);
-    std::cout << std::endl;
+
+    auto found = messages.find(msg.vchannel);
+    if (found != messages.end()) { // have to merge messages
+      if (msg.msgbytes < DebugMsg::PAYLOAD) { //output merged messages
+          std::string out(found->second + std::string(msg.data, msg.msgbytes));
+          std::cout << found->first  << "(" << out.length() << "): ";
+          std::cout.write(out.c_str(), out.length());
+          std::cout << std::endl;
+          logger.log(found->first, out);
+          messages.erase(msg.vchannel);
+      } else { // append another message
+          std::string append(found->second + std::string(msg.data, msg.msgbytes));
+          messages.insert({msg.vchannel, append});
+      }
+    } else {
+        if (msg.msgbytes < DebugMsg::PAYLOAD) {
+            std::cout << msg.vchannel << "(" << msg.msgbytes << "): ";
+            std::cout.write(msg.data, msg.msgbytes);
+            std::cout << std::endl;
+            logger.log(msg.vchannel, {msg.data});
+        } else {
+            messages.insert({msg.vchannel, std::string(msg.data)});
+        }
+    }
     debugOut.finishRecv(handle);
   }
-  
+
   return 0;
 }
