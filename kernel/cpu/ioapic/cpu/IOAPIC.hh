e208b371eba3e645120c71682362de2a4d41f5e7
Stefan Hertrampf <Stefan.Hertrampf@b-tu.de>
2017-05-20 22:51:21 +0200

    Test configuration IOAPIC

--- kernel/cpu/ioapic/cpu/IOAPIC.hh
+++ kernel/cpu/ioapic/cpu/IOAPIC.hh
@@ -43,16 +43,18 @@ public:
     BITFIELD_END
 
     BITFIELD_DEF(uint64_t, RED_TABLE_ENTRY)
-    UIntField<value_t,base_t, 0,8> intvec; // interrupt vector from 0x10 to 0xFE
+    UIntField<value_t,base_t, 0,8> intvec; // interrupt vector from 0x20 to 0xFE
     UIntField<value_t,base_t, 8,3> delmode; // 0=normal,1=lowpriority,2=system_management,4=NMI,5=INIT,7=external
     UIntField<value_t,base_t, 11,1> destmode; //0=physical, 1=logical
-    UIntField<value_t,base_t, 12,1> delivs;
+    UIntField<value_t,base_t, 12,1> delivs; // Set if this interrupt is going to be sent, but the APIC is busy
     UIntField<value_t,base_t, 13,1> intpol; //polarity: 0=high active, 1 = low active
-    UIntField<value_t,base_t, 14,1> remote_irr;
+    UIntField<value_t,base_t, 14,1> remote_irr; // response from lapic or EOI
     UIntField<value_t,base_t, 15,1> trigger_mode; //0 edge sensitive, 1 level sensitive
-    UIntField<value_t,base_t, 16,1> int_mask; //interrupt masked if 1
+    UIntField<value_t,base_t, 16,1> int_mask; //interrupt masked if 1 (to disable)
     UIntField<value_t,base_t, 17,39> reserved1;
     UIntField<value_t,base_t, 56,8> dest; // dependend on destmode: 0: apicid; 1:set of processors
+    UIntField<value_t,base_t, 0, 32> lower;
+    UIntField<value_t,base_t, 32, 32> upper;
     RED_TABLE_ENTRY() : value(0) {}
     BITFIELD_END
 
@@ -60,15 +62,19 @@ public:
         :base_address(base_address_) 
     {}
 
-    uint32_t read(Register reg) {
+    IOAPIC(size_t base_address_)
+        :base_address((char*)base_address_) 
+    {}
+
+    uint32_t read(size_t reg) {
         uint32_t volatile *ioapic = (uint32_t volatile *)base_address;
         ioapic[0] = (reg & 0xff);
-        MLOG_ERROR(mlog::boot, "IOAPIC: have read", ioapic[4], DVAR(reg));
+        MLOG_ERROR(mlog::boot, "IOAPIC: read", DVARhex(ioapic[4]), DVARhex(reg));
         return ioapic[4];
     }
 
-    void write(Register reg, uint32_t value) {
-        MLOG_ERROR(mlog::boot, "IOAPIC: write", DVAR(base_address), DVAR(reg), DVAR(value));
+    void write(size_t reg, uint32_t value) {
+        MLOG_ERROR(mlog::boot, "IOAPIC: write", DVARhex(base_address), DVARhex(reg), DVARhex(value));
         uint32_t volatile *ioapic = (uint32_t volatile *)base_address;
         ioapic[0] = (reg & 0xff);
         ioapic[4] = value;
