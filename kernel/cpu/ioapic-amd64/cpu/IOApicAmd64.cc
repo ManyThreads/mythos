da77c1bc587f0cfba3e39084edb9611a9e6c0df5
Stefan Hertrampf <hertrste@b-tu.de>
2017-07-21 03:25:58 -0400

    Removed warning in Ioapic amd. removed comments

--- kernel/cpu/ioapic-amd64/cpu/IOApicAmd64.cc
+++ kernel/cpu/ioapic-amd64/cpu/IOApicAmd64.cc
@@ -17,23 +17,19 @@ namespace mythos {
     rte_pci.intpol = 1; // low active
 
     for (size_t i = 0; i < ver.max_redirection_table + 1; i++) {
-      //ioapic.write(REG_TABLE+2*i, INT_DISABLED | (T_IRQ0 + i));
-      //ioapic.write(REG_TABLE+2*i+1, 0);
-      rte_irq.dest = 0x20 + i;
-      write(IOApic::IOREDTBL_BASE+2*i, rte_irq.lower);
-      write(IOApic::IOREDTBL_BASE+2*i+1, rte_irq.upper);
+      rte_irq.dest = 0x21 + i;
+      write(IOApic::IOREDTBL_BASE+2*i, (uint32_t) rte_irq.lower);
+      write(IOApic::IOREDTBL_BASE+2*i+1, (uint32_t) rte_irq.upper);
     }
   }
 
 
   uint32_t IOApic::read(size_t reg) {
-     base_address[0] = (reg & 0xff);
-//MLOG_ERROR(mlog::boot, "IOAPIC: read from", DVARhex((uint64_t)base_address),":",  DVARhex(base_address[4]), DVARhex(reg));
+      base_address[0] = (reg & 0xff);
       return base_address[4];
   }
 
   void IOApic::write(size_t reg, uint32_t value) {
-    //MLOG_ERROR(mlog::boot, "IOAPIC: write to", DVARhex((uint64_t)base_address),":", DVARhex(reg), DVARhex(value));
       base_address[0] = (reg & 0xff);
       base_address[4] = value;
   }
