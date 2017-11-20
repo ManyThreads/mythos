a823826f8c34b3b99cef3917cf2f895026047702
Robert Kuban <kubanrob@tu-cottbus.de>
2017-11-20 07:12:06 -0500

    New logger for idle, and more reasonable log levels.

--- kernel/cpu/idle-knc/cpu/idle.cc
+++ kernel/cpu/idle-knc/cpu/idle.cc
@@ -25,15 +25,24 @@
  */
 
 #include "cpu/idle.hh"
+#include "util/Logger.hh"
 #include "util/mstring.hh" // for memcpy
 #include "boot/memory-layout.h" // for KNC MMIO
 #include "cpu/ctrlregs.hh"
+#include "cpu/hwthreadid.hh"
 
 #define SBOX_C6_SCRATCH0 0x0000C000
 #define MSR_CC6_STATUS 0x342
 
 extern char _setup_ap_cc6;
 extern char _setup_ap_end_cc6;
+
+namespace mlog {
+#ifndef MLOG_IDLE
+#define MLOG_IDLE FilterError
+#endif
+static mlog::Logger<MLOG_IDLE> idle;
+};
 
 namespace mythos {
   namespace idle {
@@ -44,8 +53,8 @@ namespace mythos {
     {
       // copy CC6 trampoline to the right place
       memcpy(PhysPtr<char>(0x60000).log(),
-	     physPtr(&_setup_ap_cc6).log(),
-	     size_t(&_setup_ap_end_cc6 - &_setup_ap_cc6));
+       physPtr(&_setup_ap_cc6).log(),
+       size_t(&_setup_ap_end_cc6 - &_setup_ap_cc6));
       asm volatile("wbinvd");
 
       /// @todo set CC6_EIP MSR?
@@ -55,7 +64,7 @@ namespace mythos {
       *cc6 |= 0x8000; // C1-CC6 MAS (bit 15)
 
       for (unsigned i=0; i<22; i++)
-        MLOG_ERROR(mlog::boot, "idle: C6_SCRATCH", DVAR(i), DVARhex(cc6[i]));
+        MLOG_DETAIL(mlog::idle, "C6_SCRATCH", DVAR(i), DVARhex(cc6[i]));
     }
 
     NORETURN void cpu_idle_halt() SYMBOL("cpu_idle_halt");
@@ -68,13 +77,13 @@ namespace mythos {
       auto prev = coreStates[apicID/4].cc6ready.fetch_or(uint8_t(1 << (apicID%4)));
       /// @todo is this really needed if we always go into cc6?
       if ((prev | (1 << (apicID%4))) == 0xf) { // enable cc6
-	MLOG_ERROR(mlog::boot, "idle: enable CC6", DVARhex(prev), DVAR(apicID));
+        MLOG_DETAIL(mlog::idle, "enable CC6", DVARhex(prev), DVAR(apicID));
         x86::setMSR(MSR_CC6_STATUS, x86::getMSR(MSR_CC6_STATUS) | 0x1f);
       }
 
-      MLOG_ERROR(mlog::boot, "idle:", DVARhex(x86::getMSR(MSR_CC6_STATUS)));
+      MLOG_DETAIL(mlog::idle, DVARhex(x86::getMSR(MSR_CC6_STATUS)));
       auto chlt = reinterpret_cast<uint64_t volatile*>(MMIO_ADDR+SBOX_BASE+0xac0c);
-      MLOG_ERROR(mlog::boot, "idle: cores halted", DVARhex(*chlt));
+      MLOG_INFO(mlog::idle, "cores halted", DVARhex(*chlt));
 
       coreStates[apicID/4].lock = false;
       cpu_idle_halt();
@@ -82,22 +91,22 @@ namespace mythos {
 
     void wokeup(size_t /*apicID*/, size_t reason)
     {
-      MLOG_ERROR(mlog::boot, "idle:", DVARhex(x86::getMSR(MSR_CC6_STATUS)));
+      MLOG_DETAIL(mlog::idle, DVARhex(x86::getMSR(MSR_CC6_STATUS)));
       if (reason == 1) {
-	MLOG_ERROR(mlog::boot, "idle: woke up from CC6");
-	cpu_idle_halt(); // woke up from CC6 => just sleep again
+        MLOG_INFO(mlog::idle, "woke up from CC6");
+        cpu_idle_halt(); // woke up from CC6 => just sleep again
       }
     }
 
     void wokeupFromInterrupt()
     {
-      MLOG_ERROR(mlog::boot, "idle: woke up from irq");
+      MLOG_INFO(mlog::idle, "woke up from irq");
       size_t apicID = cpu::getThreadID(); // @todo hack on KNC because threadID==apicID
       while (coreStates[apicID/4].lock.exchange(true) == true);
 
       auto prev = coreStates[apicID/4].cc6ready.fetch_and(uint8_t(~(1u << (apicID%4))));
       if (prev == 0xf) { // disable cc6
-	MLOG_ERROR(mlog::boot, "idle: disable CC6", DVARhex(prev), DVAR(apicID));
+        MLOG_DETAIL(mlog::idle, "disable CC6", DVARhex(prev), DVAR(apicID));
         x86::setMSR(MSR_CC6_STATUS, x86::getMSR(MSR_CC6_STATUS) & (~0x1Ful));
       }
 
