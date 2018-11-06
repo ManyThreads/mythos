d1a28bd9d081f03589cb172d03f1891f95696859
Randolf Rotta <rottaran@b-tu.de>
2018-11-06 23:44:47 +0100

    finally linking agains musl libc, startup, tls setup and the test app work

--- kernel/runtime/crt-init/runtime/crtbegin.cc
+++ kernel/runtime/crt-init/runtime/crtbegin.cc
@@ -31,82 +31,138 @@
 #include "mythos/syscall.hh"
 #include "runtime/mlog.hh"
 #include "runtime/tls.hh"
+#include "util/elf64.hh"
 
-extern "C" typedef void (*atexit_func_t)();
-//extern "C" int atexit(atexit_func_t func);
+
+extern "C" typedef void (*func_t)();
 extern "C" [[noreturn]] void _Exit(int exit_code);
 extern "C" [[noreturn]] void exit(int exit_code=0);
 
-extern atexit_func_t __preinit_array_start[] __attribute__((weak));
-extern atexit_func_t __preinit_array_end[] __attribute__((weak));
-extern atexit_func_t __init_array_start[] __attribute__((weak));
-extern atexit_func_t __init_array_end[] __attribute__((weak));
-extern atexit_func_t __fini_array_start[] __attribute__((weak));
-extern atexit_func_t __fini_array_end[] __attribute__((weak));
-atexit_func_t _ctors_start[] __attribute__((section(".ctors"))) = {0};
-atexit_func_t _dtors_start[] __attribute__((section(".dtors"))) = {0};
-extern "C" atexit_func_t _ctors_end[];
-extern "C" atexit_func_t _dtors_end[];
-extern "C" void _init();
-extern "C" void _fini();
+// extern func_t __preinit_array_start[] __attribute__((weak));
+// extern func_t __preinit_array_end[] __attribute__((weak));
+// extern func_t __init_array_start[] __attribute__((weak));
+// extern func_t __init_array_end[] __attribute__((weak));
+// extern func_t __fini_array_start[] __attribute__((weak));
+// extern func_t __fini_array_end[] __attribute__((weak));
+// func_t _ctors_start[] __attribute__((section(".ctors"))) = {0};
+// func_t _dtors_start[] __attribute__((section(".dtors"))) = {0};
+// extern "C" func_t _ctors_end[];
+// extern "C" func_t _dtors_end[];
+// extern "C" __attribute__((weak)) void _init();
+// extern "C" __attribute__((weak)) void _fini();
+
+extern char __executable_start; //< provided by the default linker script
+
+extern "C" __attribute__((noreturn)) int __libc_start_main(int (*)(), int, char **);
+
+extern "C" int main();
+
+extern "C" void start_c(long *)
+{
+    // init TLS is done by musl libc
+    //mythos::setupInitialTLS();
+
+    // TODO make a fake environment structure, which would normally be provided by the process creator
+    // at the bottom of the stack 
+    // see http://dbp-consulting.com/tutorials/debugging/linuxProgramStartup.html
+    // http://articles.manugarg.com/aboutelfauxiliaryvectors.html
+    
+    // when initialising the stack:
+    // push end marker
+    // push environment and command line strings
+    // push padding
+    // push AT_NULL terminator
+    // push AUXV entries
+    // push nullptr for termination of environment variables
+    // push environment variables
+    // push nullptr for command line termination
+    // push command line arguments
+    // push argc
+    
+    // need AT_PHDR, AT_PHNUM, AT_PHENT, 
+    // maybe AT_RANDOM, AT_HWCAP, AT_SYSINFO, AT_PAGESZ, AT_SECURE
+    
+    // can add own entries with id>AUX_CNT and musl/libc will ignore these    
+    // for example, pointer to capability environment information, which can also be placed on the stack 
+    
+    struct Auxv 
+    {
+        Auxv(uint64_t a_type, uint64_t a_val) : a_type(a_type), a_val(a_val) {}
+        uint64_t a_type;              /* Entry type */
+        uint64_t a_val;           /* Integer value */
+    };
+    
+    mythos::elf64::Elf64Image img(&__executable_start);
+
+    struct ProgEnv 
+    {
+        ProgEnv() {}
+        long argc = 0;
+        // here the pointers to command line arguments would come, terminated by nullptr in addition to argc
+        char* argvEnd = nullptr;
+        // here the pointers to environment variables would come, terminated by nullptr
+        char* envEnd = nullptr;
+        // here the AUX vector comes
+        Auxv phdr={3, 0};
+        Auxv phent={4, 0};
+        Auxv phnum={5, 0};
+        Auxv pagesize={6, 4096};
+        Auxv secure={23, 1};
+        Auxv auxvEnd={0,0};
+    };
+    
+    ProgEnv env;
+    long* p = (long*)&env;
+    
+    env.phdr.a_val = (uint64_t)img.phdr(0);
+    env.phnum.a_val = img.phnum();
+    env.phent.a_val = img.phent();
+    
+    int argc = p[0];
+    char **argv = (char **)(p+1);
+    __libc_start_main(&main, argc, argv);
+}
 
 // see also http://stackoverflow.com/a/28981890 :(
-extern "C" void __libc_init_array()
-{
-    // init TLS
-    mythos::setupInitialTLS();
-    
-  for (size_t i = 0; i < __preinit_array_end - __preinit_array_start; i++)
-    __preinit_array_start[i]();
-  _init();
-  for (size_t i = 0; i < __init_array_end - __init_array_start; i++) {
-    __init_array_start[i]();
-  }
+// extern "C" void __libc_init_array()
+// {
+//   for (size_t i = 0; i < __preinit_array_end - __preinit_array_start; i++)
+//     __preinit_array_start[i]();
+//   _init();
+//   for (size_t i = 0; i < __init_array_end - __init_array_start; i++) {
+//     __init_array_start[i]();
+//   }
+// 
+//   for (size_t i = 1; i < _ctors_end - _ctors_start; i++) {
+//     // mlog::TextMsg<200> msg("", "init", i, (void*)_ctors_end[-i]);
+//     // mythos::syscall_debug(msg.getData(), msg.getSize());
+//     _ctors_end[-i]();
+//   }
+// }
 
-  for (size_t i = 1; i < _ctors_end - _ctors_start; i++) {
-    // mlog::TextMsg<200> msg("", "init", i, (void*)_ctors_end[-i]);
-    // mythos::syscall_debug(msg.getData(), msg.getSize());
-    _ctors_end[-i]();
-  }
-}
+// static void __libc_fini_array()
+// {
+//   for (size_t i = 1; i < _dtors_end - _dtors_start; i++) {
+//     // mlog::TextMsg<200> msg("", "init", i, (void*)_dtors_end[i]);
+//     // mythos::syscall_debug(msg.getData(), msg.getSize());
+//     _dtors_end[i]();
+//   }
+//   for (size_t i = __fini_array_end - __fini_array_start; i > 0; i--)
+//     __fini_array_start[i-1]();
+//   _fini();
+// }
 
-static void __libc_fini_array()
-{
-  for (size_t i = 1; i < _dtors_end - _dtors_start; i++) {
-    // mlog::TextMsg<200> msg("", "init", i, (void*)_dtors_end[i]);
-    // mythos::syscall_debug(msg.getData(), msg.getSize());
-    _dtors_end[i]();
-  }
-  for (size_t i = __fini_array_end - __fini_array_start; i > 0; i--)
-    __fini_array_start[i-1]();
-  _fini();
-}
 
-constexpr size_t atexit_funcs_max = 32;
-static std::atomic<size_t> atexit_funcs_count = {0};
-static atexit_func_t atexit_funcs[atexit_funcs_max];
+// void _Exit(int exit_code)
+// {
+//   asm volatile ("syscall" : : "D"(0), "S"(exit_code) : "memory");
+//   while(true);
+// }
 
-extern "C" int atexit(atexit_func_t func) noexcept
-{
-  size_t pos = atexit_funcs_count.fetch_add(1);
-  if (pos >= atexit_funcs_max) {
-    atexit_funcs_count = atexit_funcs_max;
-    return -1;
-  }
-  atexit_funcs[pos] = func;
-  return 0;
-}
-
-void _Exit(int exit_code)
-{
-  asm volatile ("syscall" : : "D"(0), "S"(exit_code) : "memory");
-  while(true);
-}
-
-void exit(int exit_code)
-{
-  for (size_t i = 0; i < atexit_funcs_count; i++)
-    if (atexit_funcs[i]) atexit_funcs[i]();
-  __libc_fini_array();
-  _Exit(exit_code);
-}
+// void exit(int exit_code)
+// {
+// //  for (size_t i = 0; i < atexit_funcs_count; i++)
+// //    if (atexit_funcs[i]) atexit_funcs[i]();
+//   __libc_fini_array();
+//   _Exit(exit_code);
+// }
--- kernel/runtime/cxx/runtime/cxxsupport.cc
+++ kernel/runtime/cxx/runtime/cxxsupport.cc
@@ -44,46 +44,6 @@ extern "C" [[noreturn]] void __assert_fail (const char *expr, const char *file, 
 }
 
 
-#if __BYTE_ORDER == __BIG_ENDIAN
-#define X(x) x
-#else
-#define X(x) (((x)/256 | (x)*256) % 65536)
-#endif
-
-// from musl libc
-static const unsigned short table[] = {
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),
-X(0x200),X(0x320),X(0x220),X(0x220),X(0x220),X(0x220),X(0x200),X(0x200),
-X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),
-X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),
-X(0x160),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
-X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
-X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),
-X(0x8d8),X(0x8d8),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
-X(0x4c0),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8c5),
-X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),
-X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),
-X(0x8c5),X(0x8c5),X(0x8c5),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
-X(0x4c0),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8c6),
-X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),
-X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),
-X(0x8c6),X(0x8c6),X(0x8c6),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x200),
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-};
-
-static const unsigned short *const ptable = table+128;
-
-// returns an pointer to an array!
-extern "C" const unsigned short **__ctype_b_loc() { return (const unsigned short **)&ptable; }
-
-
 // undefined reference to `__memmove_chk'
 // undefined reference to `pthread_getspecific'
 // undefined reference to `pthread_key_create'
@@ -95,161 +55,23 @@ extern "C" const unsigned short **__ctype_b_loc() { return (const unsigned short
 // undefined reference to `__vfprintf_chk'
 
 
-/** cause abnormal process termination.
- * Defined in stdlib.h
- */
-extern "C" void abort() {
-    MLOG_ERROR(mlog::app, "abort");
-    while(true) mythos::syscall_exit(-1); 
-}
-
-struct FILE;
-
-FILE* stdin  = (FILE*)0x47110000;
-FILE* stdout = (FILE*)0x47110001;
-FILE* stderr = (FILE*)0x47110002;
-
-extern "C" int fflush(FILE* stream) {
-    MLOG_ERROR(mlog::app, "fflush", DVAR(stream));
-    return 0;
-}
-
-extern "C" int fprintf(FILE *stream, const char *format, ...) {
-    MLOG_ERROR(mlog::app, "fprintf", DVAR(stream), DSTR(format));
-    return 0;
-}
-
-extern "C" int snprintf(char *str, size_t size, const char *format, ...) {
-    MLOG_ERROR(mlog::app, "snprintf", DVAR(str), DVAR(size), DSTR(format));
-    str[0] = '\0';
-    return 0;
-}
-
-extern "C" int vfprintf(FILE *stream, const char *format, va_list) {
-    MLOG_ERROR(mlog::app, "vfprintf", DVAR(stream), DSTR(format));
-    return 0;
-}
-
-extern "C" int fputc(int c, FILE *stream) {
-    MLOG_ERROR(mlog::app, "fputc", DVAR(stream), DVAR(c));
-    return 0;
-}
-
-extern "C" size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    MLOG_ERROR(mlog::app, "fwrite", DVAR(stream), mlog::DebugString((const char*)ptr,size*nmemb), DVAR(size), DVAR(nmemb));
-    return nmemb;
-}
-
-extern "C" char *getenv(const char *name) {
-    MLOG_ERROR(mlog::app, "getenv", DSTR(name));
-    return nullptr;
-}
-
-extern "C" int __fprintf_chk(FILE * stream, int /*flag*/, const char * format) {
-    return fprintf(stream, format);
-}
-
-extern "C" int __snprintf_chk(char * str, size_t maxlen, int flag, size_t strlen, const char * format)
+extern "C" long mythos_musl_syscall(long num, long a1, long a2, long a3,
+	             long a4, long a5, long a6)
 {
-    str[0] = '\0';
-    return strlen;
-}
-
-extern "C" int __vfprintf_chk(FILE * fp, int flag, const char * format, va_list ap) {
-    return vfprintf(fp,format, ap);
-}
-
-extern "C" int isupper(int c) {
-  unsigned char x=c&0xff;
-  return (x>='A' && x<='Z') || (x>=192 && x<=222 && x!=215);
-}
-
-extern "C" int isxdigit(int ch) {
-    return (unsigned int)( ch         - '0') < 10u  || 
-           (unsigned int)((ch | 0x20) - 'a') <  6u;
-}
-
-extern "C" int pthread_once(pthread_once_t *once_control, 
-    void (*init_routine)(void))
-{
-    MLOG_ERROR(mlog::app, "pthread_once", DVAR(once_control), DVAR((void*)init_routine));
-    auto& m = *reinterpret_cast<std::atomic<int>*>(once_control);
-    auto old = m.exchange(1);
-    if (old == 0) (*init_routine)();
-    return 0;
-}
-
-struct DummyMutex
-{
-    std::atomic<int> flag;
-};
-
-extern "C" int pthread_mutex_lock(pthread_mutex_t *mutex)
-{
-    MLOG_ERROR(mlog::app, "pthread_mutex_lock", DVAR(mutex));
-    auto m = reinterpret_cast<DummyMutex*>(mutex);
-    while (m->flag.exchange(1) != 0);
-    return 0;
-}
-
-extern "C" int pthread_mutex_trylock(pthread_mutex_t *mutex)
-{
-    MLOG_ERROR(mlog::app, "pthread_mutex_trylock", DVAR(mutex));
-    auto m = reinterpret_cast<DummyMutex*>(mutex);
-    if (m->flag.exchange(1) == 0) return 0;
+    switch (num) {
+    case 60: // exit(exit_code)
+        asm volatile ("syscall" : : "D"(0), "S"(a1) : "memory");
+        return 0;
+    case 231: // exit_group for all pthreads 
+        return -1;
+    default:
+        MLOG_ERROR(mlog::app, "mythos_musl_syscall", DVAR(num), 
+            DVAR(a1), DVAR(a2), DVAR(a3),
+            DVAR(a4), DVAR(a5), DVAR(a6));
+    }
     return -1;
 }
 
-extern "C" int pthread_mutex_unlock(pthread_mutex_t *mutex)
-{
-    MLOG_ERROR(mlog::app, "pthread_mutex_unlock", DVAR(mutex));
-    auto m = reinterpret_cast<DummyMutex*>(mutex);
-    auto old = m->flag.exchange(0);
-    ASSERT(old == 0);
-    return 0;
-}
-
-extern "C" int pthread_rwlock_rdlock(pthread_rwlock_t* rwlock) {
-    MLOG_ERROR(mlog::app, "pthread_rwlock_rdlock", DVAR(rwlock));
-    return 0;
-}
-
-extern "C" int pthread_rwlock_unlock(pthread_rwlock_t *rwlock) {
-    MLOG_ERROR(mlog::app, "pthread_rwlock_unlock", DVAR(rwlock));
-    return 0;
-}
-
-extern "C" int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock) {
-    MLOG_ERROR(mlog::app, "pthread_rwlock_wrlock", DVAR(rwlock));
-    return 0;
-}
-
-namespace { 
-    thread_local void* threadDataTable[64]; 
-    std::atomic<int> freeEntry = {0};
-}
-
-extern "C" void *pthread_getspecific(pthread_key_t key)
-{
-    MLOG_ERROR(mlog::app, "pthread_getspecific", DVAR(key));
-    return threadDataTable[key];
-}
-
-extern "C" int pthread_setspecific(pthread_key_t key, const void *value)
-{
-    MLOG_ERROR(mlog::app, "pthread_setspecific", DVAR(key), DVAR(value));
-    threadDataTable[key] = (void*)value;
-    return 0;
-}
-
-extern "C" int pthread_key_create(pthread_key_t *key, void (*destructor)(void*))
-{
-    MLOG_ERROR(mlog::app, "pthread_key_create", DVAR(key), DVAR((void*)destructor));
-    ASSERT(freeEntry < 64);
-    auto prev = freeEntry.fetch_add(1);
-	*key = prev;
-    return 0;
-}
 
 struct dl_phdr_info
 {
@@ -294,78 +116,3 @@ extern "C" int dl_iterate_phdr(
     auto res = (*callback) (&info, sizeof(info), data);
     return res;
 }
-
-// http://wiki.osdev.org/C%2B%2B
-void* __dso_handle = 0;
-
-struct Dl_info
-{
-    const char *dli_fname;  /* Pathname of shared object that contains address */
-    void       *dli_fbase;  /* Base address at which shared object is loaded */
-    const char *dli_sname;  /* Name of symbol whose definition overlaps addr */
-    void       *dli_saddr;  /* Exact address of symbol named in dli_sname */
-};
-
-// used only for debugging by libcxxabi ???
-/** translate address to symbolic information */
-extern "C" int dladdr(void* addr, Dl_info* info) {
-    MLOG_ERROR(mlog::app, "dladdr", DVAR(addr), DVAR(info));
-	return 0;
-}
-
-// http://refspecs.linuxbase.org/LSB_3.1.1/LSB-Core-generic/LSB-Core-generic/baselib---cxa-atexit.html
-// http://wiki.osdev.org/C%2B%2B
-/** register a function to be called by exit or when a shared library is unloaded */
-extern "C" int __cxa_atexit(void (*func)(void*), void* arg, void* dso_handle) {
-    MLOG_ERROR(mlog::app, "atexit", DVAR((void*)func), DVAR(arg), DVAR(dso_handle));
-    return 0; // success
-}
-
-// http://wiki.osdev.org/C%2B%2B
-// http://refspecs.linuxbase.org/LSB_5.0.0/LSB-Core-generic/LSB-Core-generic/baselib---cxa-finalize.html
-/** call destructors of global (or local static) C++ objects and
- * exit functions registered with atexit */
-extern "C" void __cxa_finalize(void* d) { 
-    MLOG_ERROR(mlog::app, "finalize", DVAR(d));
-}
-
-// http://wiki.osdev.org/C%2B%2B
-// implemented by libcxxabi
-// extern "C" void __cxa_pure_virtual() { while(true) mythos::syscall_exit(-1); }
-
-// http://refspecs.linux-foundation.org/LSB_4.1.0/LSB-Core-generic/LSB-Core-generic/libc---stack-chk-fail-1.html
-/** terminate a function in case of stack overflow. */
-extern "C" void __stack_chk_fail() {
-    /// @todo does this even make sense, when we already have a stack overflow?
-    MLOG_ERROR(mlog::app, "stack_chk_fail");
-    while(true) mythos::syscall_exit(-1); 
-}
-
-// implemented by libcxxabi
-// http://mentorembedded.github.io/cxx-abi/abi.html#member-pointers
-// http://wiki.osdev.org/C%2B%2B#Local_Static_Variables_.28GCC_Only.29
-// http://www.opensource.apple.com/source/libcppabi/libcppabi-14/src/cxa_guard.cxx
-// namespace __cxxabiv1
-// {
-//   /* guard variables */
-// 
-//   /* The ABI requires a 64-bit type.  */
-//   __extension__ typedef uint64_t __guard __attribute__((mode(__DI__)));
-// 
-//   extern "C" int __cxa_guard_acquire (__guard *);
-//   extern "C" void __cxa_guard_release (__guard *);
-//   extern "C" void __cxa_guard_abort (__guard *);
-// 
-//   extern "C" int __cxa_guard_acquire (__guard *g)
-//   {
-//     return !*(char *)(g);
-//   }
-// 
-//   extern "C" void __cxa_guard_release (__guard *g)
-//   {
-//     *(char *)g = 1;
-//   }
-// 
-//   extern "C" void __cxa_guard_abort (__guard *)
-//   {}
-// }
